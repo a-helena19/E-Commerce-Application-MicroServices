4fbfba8d3e06c43575e1f58732f8e22449acfb9f
a-helena19 <helenaaldlol@gmail.com>
2026-03-17 15:51:44 +0100

    initialize microservices structure with Api Gateway, Cart Service, User Service, Order Service, and Product Service

---
+++ Cart Service/src/test/java/at/fhv/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
