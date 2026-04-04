721f7bff4fc451334b1d9f7884352294f715b6c2
a-helena19 <helenaaldlol@gmail.com>
2026-04-04 15:16:59 +0200

    initialize Composition Service with Gradle setup and basic application structure

--- Composition Service/src/main/java/at/fhv/compositionservice/CompositionServiceApplication.java
+++ Composition Service/src/main/java/at/fhv/compositionservice/CompositionServiceApplication.java
@@ -5,20 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@CrossOrigin
 public class CompositionServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CompositionServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CompositionServiceApplication.class, args);
+	}
 
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 }
